86b2fa45bb3701487cda51481e8ec1154cc36eb5
박영수 <yspark@avatye.com>
2024-05-24 19:37:01 +0900

    CashButton SDK update(v3.0.3.303)

--- sample_kotlin_cashbutton/cashbutton_2_0_s2s/src/main/java/com/sudal/cashbutton_2_0_s2s/Application.kt
+++ sample_kotlin_cashbutton/cashbutton_2_0_s2s/src/main/java/com/sudal/cashbutton_2_0_s2s/Application.kt
@@ -19,7 +19,7 @@ class Application : Application() {
          *  appID = "91e4b7f81a6511ea813d0a4916b2361a", appSecret = "aafc702323bf6a214"
          *
          *  캐시버튼 채널링 테스트 키
-         *  appID = "69f43822b64d46d6bc01ff6d282f754d", appSecret = "077b11dc468f4bd3"
+         *  appID = "2f7dc58210b646e4bae2c6409dcb4a27", appSecret = "1855684419894d5e"
          *
          *  채널링의 경우 CashButtonChannelingActivity에서 phoneNumber를 반드시 설정해주셔야 하며,
          *  서비스 변경 시 설치된 앱 삭제 후 새로 설치해주세요.
@@ -27,7 +27,10 @@ class Application : Application() {
 
 
         CashButtonSDK(
-            application = this, appId = "69f43822b64d46d6bc01ff6d282f754d", appSecret = "077b11dc468f4bd3"
+            application = this,
+            appId = "2f7dc58210b646e4bae2c6409dcb4a27",
+            appSecret = "1855684419894d5e",
+            isAppTestMode = true
         ).apply {
             // debug-mode
             setUseDebug(use = true)
