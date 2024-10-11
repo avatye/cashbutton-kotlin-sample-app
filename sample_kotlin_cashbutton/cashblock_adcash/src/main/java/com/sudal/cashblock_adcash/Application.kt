8673c1120f9f9d36e3d830a527be3deec8300a17
박영수 <yspark@avatye.com>
2024-10-11 21:12:41 +0900

    <Cashbutton_ADCAsh> SDK update(2.1.0.20)

--- sample_kotlin_cashbutton/cashblock_adcash/src/main/java/com/sudal/cashblock_adcash/Application.kt
+++ sample_kotlin_cashbutton/cashblock_adcash/src/main/java/com/sudal/cashblock_adcash/Application.kt
@@ -12,14 +12,14 @@ class Application : Application() {
         /** 발급받은 서비스 키가 없다면 테스트 키로 적용해주세요.
          *
          *  애드캐시 테스트 키
-         *  appID = "98d4d4c35d594451b21f54718e2bc986", appSecret = "c395dbe200ad4493ade96fb92c988fcf1c8df2d3687d49a9ab6f31f7c05e2bf4"
+         *  appID = "6da4ad56527f409d9e3f97700437abc0", appSecret = "09ff5525d0e44ed8"
          *
          */
 
         ADCashSDK.initialize(
             context = this,
-            appId = "98d4d4c35d594451b21f54718e2bc986",
-            appSecret = "c395dbe200ad4493ade96fb92c988fcf1c8df2d3687d49a9ab6f31f7c05e2bf4",
+            appId = "6da4ad56527f409d9e3f97700437abc0",
+            appSecret = "09ff5525d0e44ed8",
             storeUrl = "https://www.avatye.com"
         )
     }
